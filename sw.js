0fef2eb0825c4ed0a15eb1d7817672bf9c8cc8fc
tiemah <doreentiema87@gmail.com>
2023-07-01 11:33:40 +0300

    debbugging

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ self.addEventListener('install', event => {
   event.waitUntil((async () => {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
-      '/PWA/',
-      '/PWA/converter.js',
-      '/PWA/converter.css',
+      './',
+      './converter.js',
+      './converter.css',
       
     ]);
   })());
